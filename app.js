1c13a9936dd80e5f0e21484e7c5a20d9ba1fe790
saida brito <saidabrito57@gmail.com>
2020-06-18 17:02:54 -0500

    Added User see a list of added notes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const main = document.getElementsByClassName('main');
-
+const mainContainer = document.querySelector(".main");
+const allNotes = document.querySelector(".all-notes");
 const notes = [];
 
 // AÑADIR NOTAS
@@ -23,4 +23,42 @@ function addNote(title = '', body, color, trash = false, pinned = false) {
   console.log(notes);
 }
 
-noteSubmit.addEventListener('click', addNote);
+noteSubmit.addEventListener('click', () => {addNote(); showNotes()});
+
+
+function createNote(note) {
+  const noteContainer = document.createElement("div");
+  noteContainer.classList.add('note-container');
+  allNotes.insertBefore(noteContainer, allNotes.firstChild);
+
+  const noteTitleContainer = document.createElement("p");
+  noteTitleContainer.className = 'note-title-content';
+  noteTitleContainer.textContent = note.title; 
+  noteContainer.appendChild(noteTitleContainer);
+
+  const noteBodyContainer = document.createElement("p");
+  noteBodyContainer.className = 'note-body-content';
+  noteBodyContainer.textContent = note.body; 
+  noteContainer.appendChild(noteBodyContainer);
+
+  const iconsContainer = document.createElement('div');
+  iconsContainer.className = 'icons-container';
+  noteContainer.appendChild(iconsContainer);
+
+  const paletteImage = document.createElement('img');
+  paletteImage.src ='images/Frame 7.svg'; 
+  iconsContainer.appendChild(paletteImage);
+
+  const trashImage = document.createElement('img');
+  trashImage.src = 'images/Frame 8.svg'; 
+  iconsContainer.appendChild(trashImage);
+  
+  return noteContainer; 
+}
+
+function showNotes(){
+  const currentNote = notes.pop();
+  createNote(currentNote); 
+}
+
+
