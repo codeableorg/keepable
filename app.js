664fb0ad4f53b872cae1c59963b390bfa95d1120
saida brito <saidabrito57@gmail.com>
2020-06-19 07:56:04 -0500

    added background color when create a note

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ const allNotes = document.querySelector(".all-notes");
 const notes = [];
 
 // AÑADIR NOTAS
-const noteSubmit = document.getElementById('note-submit');
+const noteSubmit = document.getElementById("note-submit");
 
 const colorButton = document.querySelector(".new-note__color");
 const paletteColors = document.querySelector(".palette-colors");
 const newNoteWrapper = document.getElementById("new-note");
 
-function addNote(title = '', body, color, trash = false, pinned = false) {
-  const noteTitle = document.getElementById('note-title').value;
-  const noteBody = document.getElementById('note-body').value;
+function addNote(color, trash = false, pinned = false) {
+  const noteTitle = document.getElementById("note-title").value;
+  const noteBody = document.getElementById("note-body").value;
 
   const note = {
     title: noteTitle,
     body: noteBody,
-    color: 'white',
+    color: newNoteWrapper.style.backgroundColor,
     trash: this.trash,
     pinned: this.pinned,
     updatedAt: Date.now(),
@@ -27,59 +27,65 @@ function addNote(title = '', body, color, trash = false, pinned = false) {
   console.log(notes);
 }
 
-noteSubmit.addEventListener('click', () => {addNote(); showNotes(); 
-  newNoteWrapper.style.backgroundColor = '#ffffff'; 
+noteSubmit.addEventListener("click", () => {
+  addNote();
+  showNotes();
+  newNoteWrapper.style.backgroundColor = "#ffffff";
 });
 
 //show list of notes
 function createNote(note) {
   const noteContainer = document.createElement("div");
-  noteContainer.classList.add('note-container');
+  noteContainer.classList.add("note-container");
+  noteContainer.style.backgroundColor = note.color;
   allNotes.insertBefore(noteContainer, allNotes.firstChild);
 
   const noteTitleContainer = document.createElement("p");
-  noteTitleContainer.className = 'note-title-content';
-  noteTitleContainer.textContent = note.title; 
+  noteTitleContainer.className = "note-title-content";
+  noteTitleContainer.textContent = note.title;
   noteContainer.appendChild(noteTitleContainer);
 
   const noteBodyContainer = document.createElement("p");
-  noteBodyContainer.className = 'note-body-content';
-  noteBodyContainer.textContent = note.body; 
+  noteBodyContainer.className = "note-body-content";
+  noteBodyContainer.textContent = note.body;
   noteContainer.appendChild(noteBodyContainer);
 
-  const iconsContainer = document.createElement('div');
-  iconsContainer.className = 'icons-container';
+  const iconsContainer = document.createElement("div");
+  iconsContainer.className = "icons-container";
   noteContainer.appendChild(iconsContainer);
 
-  const paletteImage = document.createElement('img');
-  paletteImage.src ='images/Frame 7.svg'; 
+  const paletteImage = document.createElement("img");
+  paletteImage.src = "images/Frame 7.svg";
   iconsContainer.appendChild(paletteImage);
 
-  const trashImage = document.createElement('img');
-  trashImage.src = 'images/Frame 8.svg'; 
+  const trashImage = document.createElement("img");
+  trashImage.src = "images/Frame 8.svg";
   iconsContainer.appendChild(trashImage);
-  
-  return noteContainer; 
+
+  return noteContainer;
 }
 
-function showNotes(){
+function showNotes() {
   const currentNote = notes.pop();
-  createNote(currentNote); 
+  console.log(currentNote.color);
+  createNote(currentNote);
 }
 
-//User can include custom color while creating a note 
+//User can include custom color while creating a note y ya se agrega color a la nota que se crea. 
 
-function changeNoteColor(){
-  colorButton.addEventListener("click", () =>  paletteColors.classList.toggle("hidde")); 
+function openPaletteColor() {
+  colorButton.addEventListener("click", () =>
+    paletteColors.classList.toggle("hidde")
+  );
 }
 
-changeNoteColor(); 
+openPaletteColor();
 
-document.querySelectorAll(".color").forEach((color) => color.addEventListener("click", (event) => {
-  const buttonSelected = event.target;
-  const color = buttonSelected.dataset.color;
-  newNoteWrapper.style.backgroundColor = color; 
-  paletteColors.classList.toggle("hidde");
-})); 
-
-
+document.querySelectorAll(".color").forEach((color) =>
+  color.addEventListener("click", (event) => {
+    const buttonSelected = event.target;
+    const color = buttonSelected.dataset.color;
+    newNoteWrapper.style.backgroundColor = color;
+    paletteColors.classList.toggle("hidde");
+  })
+);
