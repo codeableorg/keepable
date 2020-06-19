eb72942734eafb8807acbb4311321c6422bc5551
NicolleQuispeArotinco <nicolleqa3@gmail.com>
2020-06-18 21:41:52 -0500

    resolve conflicts

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 class Note {
   constructor(content, color) {
@@ -8,14 +8,14 @@ class Note {
 }
 
 let state = {
-  main: document.querySelector('main'),
+  main: document.querySelector("main"),
   normal_notes: [],
   trashed_notes: [],
-  add_note (description, color = 'red') {
+  add_note(description, color = "red") {
     this.normal_notes.push(new Note(description, color));
   },
-  remove_normal_note(id){
-    if(id >= this.normal_notes.length){
+  remove_normal_note(id) {
+    if (id >= this.normal_notes.length) {
       console.log("Id no valido");
       return;
     }
@@ -23,16 +23,16 @@ let state = {
     this.trashed_notes.push(normal_note);
     this.render_note_page();
   },
-  change_color_of_normal_note(id, color){
-    if(id >= this.normal_notes.length){
+  change_color_of_normal_note(id, color) {
+    if (id >= this.normal_notes.length) {
       console.log("Id no valido");
       return;
     }
     this.normal_notes[id].color = color;
     this.render_note_page();
   },
-  remove_trashed_note(id){
-    if(id >= this.trashed_notes.length){
+  remove_trashed_note(id) {
+    if (id >= this.trashed_notes.length) {
       console.log("Id no valido");
       return;
     }
@@ -40,24 +40,26 @@ let state = {
     this.render_note_page();
   },
 
-  render_note_page(){
-    new Promise(function(resolve, reject) {
+  render_note_page() {
+    new Promise(function (resolve, reject) {
       resolve(new DocumentFragment());
-    }).then((fragment) => {
-      return this.create_form(fragment);
-    }).then((fragment) => {
-      return this.generate_notes(fragment);
-    }).then((fragment) => {
-      this.main.innerHTML = "";
-      this.main.append(fragment);
-    });
+    })
+      .then((fragment) => {
+        return this.create_form(fragment);
+      })
+      .then((fragment) => {
+        return this.generate_notes(fragment);
+      })
+      .then((fragment) => {
+        this.main.innerHTML = "";
+        this.main.append(fragment);
+      });
   },
 
   create_form: function (fragment) {
-    let div = document.createElement('div');
-    div.className = 'new_note_container';
-    div.innerHTML = 
-      `<form action="" class="new_note">
+    let div = document.createElement("div");
+    div.className = "new_note_container";
+    div.innerHTML = `<form action="" class="new_note">
         <textarea name="" id="textarea" cols="30" rows="10" placeholder="Some great think!"></textarea> 
         <button>Keep it!</button>
       </form>`;
@@ -65,23 +67,22 @@ let state = {
     return fragment;
   },
 
-  generate_notes : function (fragment){ 
-    let notes_container = document.createElement('div');
-    notes_container.className = 'notes_container';
+  generate_notes: function (fragment) {
+    let notes_container = document.createElement("div");
+    notes_container.className = "notes_container";
 
     this.normal_notes.forEach(function (note) {
-      let note_element = document.createElement('div');
-      note_element.className = 'note';
+      let note_element = document.createElement("div");
+      note_element.className = "note";
       note_element.innerText = `content ${note.content}, color ${note.color}`;
-      notes_container.append(note_element)
+      notes_container.append(note_element);
     });
     fragment.append(notes_container);
     return fragment;
-  }
-  
+  },
 };
 
-state.add_note('holi');
-state.add_note('woli', 'blue');
+state.add_note("holi");
+state.add_note("woli", "blue");
 
-state.render_note_page()
+state.render_note_page();
