cee7acd1b36e30d9750470ef0809f68ac9c23862
Andrés Del Carpio <andresdc@andresdc.net>
2020-06-18 23:09:20 -0500

    Added Notes Helper functions

---
+++ notesHelpers.js
@@ -0,0 +1,16 @@
+function emptyNotes() {
+  notes = [];
+}
+
+function populateNotes() {
+  addNote('Title', 'This is the body for the note.', 'white');
+  addNote('Title', 'This is the body for the note.', 'red');
+  addNote('Title', 'This is the body for the note.', 'orange');
+  addNote('Title', 'This is the body for the note.', 'yellow');
+  addNote('Title', 'This is the body for the note.', 'green');
+  addNote('Title', 'This is the body for the note.', 'cyan');
+  addNote('Title', 'This is the body for the note.', 'lighblue');
+  addNote('Title', 'This is the body for the note.', 'blue');
+  addNote('Title', 'This is the body for the note.', 'purple');
+  addNote('Title', 'This is the body for the note.', 'pink');
+}
