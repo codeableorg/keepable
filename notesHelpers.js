ebcb7169aaa9c8ebc9c217c3ce6bcb2b4bca191f
Andrés Del Carpio <andresdc@andresdc.net>
2020-06-18 23:24:26 -0500

    Fixed notes helpers

--- notesHelpers.js
+++ notesHelpers.js
@@ -3,14 +3,84 @@ function emptyNotes() {
 }
 
 function populateNotes() {
-  addNote('Title', 'This is the body for the note.', 'white');
-  addNote('Title', 'This is the body for the note.', 'red');
-  addNote('Title', 'This is the body for the note.', 'orange');
-  addNote('Title', 'This is the body for the note.', 'yellow');
-  addNote('Title', 'This is the body for the note.', 'green');
-  addNote('Title', 'This is the body for the note.', 'cyan');
-  addNote('Title', 'This is the body for the note.', 'lighblue');
-  addNote('Title', 'This is the body for the note.', 'blue');
-  addNote('Title', 'This is the body for the note.', 'purple');
-  addNote('Title', 'This is the body for the note.', 'pink');
+  notes.push({
+    title: 'Title',
+    body: 'This is the body for the note.',
+    color: 'white',
+    trash: true,
+    pinned: false,
+    updatedAt: Date.now(),
+  });
+  notes.push({
+    title: 'Title',
+    body: 'This is the body for the note.',
+    color: 'red',
+    trash: false,
+    pinned: false,
+    updatedAt: Date.now(),
+  });
+  notes.push({
+    title: 'Title',
+    body: 'This is the body for the note.',
+    color: 'orange',
+    trash: false,
+    pinned: false,
+    updatedAt: Date.now(),
+  });
+  notes.push({
+    title: 'Title',
+    body: 'This is the body for the note.',
+    color: 'yellow',
+    trash: false,
+    pinned: false,
+    updatedAt: Date.now(),
+  });
+  notes.push({
+    title: 'Title',
+    body: 'This is the body for the note.',
+    color: 'green',
+    trash: false,
+    pinned: true,
+    updatedAt: Date.now(),
+  });
+  notes.push({
+    title: 'Title',
+    body: 'This is the body for the note.',
+    color: 'cyan',
+    trash: true,
+    pinned: false,
+    updatedAt: Date.now(),
+  });
+  notes.push({
+    title: 'Title',
+    body: 'This is the body for the note.',
+    color: 'lightblue',
+    trash: false,
+    pinned: false,
+    updatedAt: Date.now(),
+  });
+  notes.push({
+    title: 'Title',
+    body: 'This is the body for the note.',
+    color: 'blue',
+    trash: false,
+    pinned: false,
+    updatedAt: Date.now(),
+  });
+  notes.push({
+    title: 'Title',
+    body: 'This is the body for the note.',
+    color: 'purple',
+    trash: false,
+    pinned: true,
+    updatedAt: Date.now(),
+  });
+  notes.push({
+    title: 'Title',
+    body: 'This is the body for the note.',
+    color: 'pink',
+    trash: false,
+    pinned: false,
+    updatedAt: Date.now(),
+  });
 }
