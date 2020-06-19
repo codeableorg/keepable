7d7fa5703a92c1c9e5097b6c26cfaff2eb7025c7
humberto delgado <dehumberto@gmail.com>
2020-06-19 15:40:22 -0500

    select a note to delete

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const mainContainer = document.querySelector('.main');
-const allNotes = document.querySelector('.all-notes');
+const mainContainer = document.querySelector(".main");
+const allNotes = document.querySelector(".all-notes");
 let notes = [];
 
 // AÑADIR NOTAS
-const noteSubmit = document.getElementById('note-submit');
+const noteSubmit = document.getElementById("note-submit");
 
-const colorButton = document.querySelector('.new-note__color');
-const paletteColors = document.querySelector('.palette-colors');
-const newNoteWrapper = document.getElementById('new-note');
+const colorButton = document.querySelector(".new-note__color");
+const paletteColors = document.querySelector(".palette-colors");
+const newNoteWrapper = document.getElementById("new-note");
 
 function addNote(color, trash = false, pinned = false) {
-  const noteTitle = document.getElementById('note-title').value;
-  const noteBody = document.getElementById('note-body').value;
+  const noteTitle = document.getElementById("note-title").value;
+  const noteBody = document.getElementById("note-body").value;
 
   const note = {
     title: noteTitle,
@@ -28,57 +28,71 @@ function addNote(color, trash = false, pinned = false) {
 
 // clears all the contents inside a div
 function clearElement(element) {
-  element.innerHTML = '';
+  element.innerHTML = "";
 }
 
 function removeChild(parent, child) {
   parent.removeChild(child);
 }
 
-noteSubmit.addEventListener('click', () => {
-  if (document.querySelector('.no-notes')) {
-    removeChild(mainContainer, document.querySelector('.no-notes'));
+noteSubmit.addEventListener("click", () => {
+  if (document.querySelector(".no-notes")) {
+    removeChild(mainContainer, document.querySelector(".no-notes"));
   }
   clearElement(allNotes);
   addNote();
   createAllNotes(notes);
-  newNoteWrapper.style.backgroundColor = '#ffffff';
+  newNoteWrapper.style.backgroundColor = "#ffffff";
 });
 
 //show list of notes
 function createNote(note) {
-  const noteContainer = document.createElement('article');
-  noteContainer.classList.add('note');
-  noteContainer.classList.add('border-shadow');
+  const noteContainer = document.createElement("article");
+  noteContainer.classList.add("note");
+  noteContainer.classList.add("border-shadow");
   noteContainer.style.backgroundColor = note.color;
   allNotes.insertBefore(noteContainer, allNotes.firstChild);
 
-  const noteTitleContainer = document.createElement('h1');
-  noteTitleContainer.className = 'note__title';
-  noteTitleContainer.id = 'note__title';
+  const noteTitleContainer = document.createElement("h1");
+  noteTitleContainer.className = "note__title";
+  noteTitleContainer.id = "note__title";
   noteTitleContainer.textContent = note.title;
   noteContainer.appendChild(noteTitleContainer);
 
-  const noteBodyContainer = document.createElement('p');
-  noteBodyContainer.className = 'note__body';
-  noteBodyContainer.id = 'note__body';
+  const noteBodyContainer = document.createElement("p");
+  noteBodyContainer.className = "note__body";
+  noteBodyContainer.id = "note__body";
   noteBodyContainer.textContent = note.body;
   noteContainer.appendChild(noteBodyContainer);
 
-  const iconsContainer = document.createElement('div');
-  iconsContainer.className = 'icons-container';
+  const iconsContainer = document.createElement("div");
+  iconsContainer.className = "icons-container";
   noteContainer.appendChild(iconsContainer);
 
-  const paletteImage = document.createElement('img');
-  paletteImage.src = 'images/Frame 7.svg';
+  const paletteImage = document.createElement("img");
+  paletteImage.src = "images/Frame 7.svg";
   iconsContainer.appendChild(paletteImage);
 
-  const trashImage = document.createElement('img');
-  trashImage.src = 'images/Frame 8.svg';
+  const trashImage = document.createElement("img");
+  trashImage.src = "images/Frame 8.svg";
+  trashImage.classList.add("button-trash");
   iconsContainer.appendChild(trashImage);
 
   return noteContainer;
 }
+
+function selectOneNote(notes, title) {
+  let selectedNote = notes.filter((note) => note.title === title);
+  console.log(selectedNote);
+}
+
+buttonTrash.addEventListener("click", () => {
+  const buttonTrash = event.target;
+  const currentNoteContainer = buttonTrash.event.target.closest(".note");
+  console.log(notes);
+  console.log(currentNoteContainer.value);
+  //selectOneNote(notes,)
+});
 
 // function to show all notes
 function createAllNotes(notes) {
@@ -96,16 +110,18 @@ function createAllNotes(notes) {
 
 //User can include custom color while creating a note y ya se agrega color a la nota que se crea.
 function openPaletteColor() {
-  colorButton.addEventListener('click', () => paletteColors.classList.toggle('hidden'));
+  colorButton.addEventListener("click", () =>
+    paletteColors.classList.toggle("hidden")
+  );
 }
 
 openPaletteColor();
 
-document.querySelectorAll('.color').forEach((color) =>
-  color.addEventListener('click', (event) => {
+document.querySelectorAll(".color").forEach((color) =>
+  color.addEventListener("click", (event) => {
     const buttonSelected = event.target;
     const color = buttonSelected.dataset.color;
     newNoteWrapper.style.backgroundColor = color;
-    paletteColors.classList.toggle('hidden');
+    paletteColors.classList.toggle("hidden");
   })
 );
