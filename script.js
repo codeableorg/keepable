d56c8dd8e142548fb506f70fe7f8a8e7e150ff82
Alessandro Chumpitaz <par_214@hotmail.com>
2020-06-18 20:59:47 -0500

    Added Features

--- script.js
+++ script.js
@@ -6,24 +6,23 @@ class Card {
         this.pinned = false;
     }
     html() {
-	    return (
-		    `
+	    return `
 		    <div class="card">
-		            <div class="title">
-				${this.title}
-		            </div>
-		            <div class="content">
-				${this.content}
-		            </div>
-			    <input type="color"></input>
-		            <button onclick="app.delete_note(${this.id})" class="delete">Delete</button>
+		    	<div class="title">
+					${this.title}
+		        </div>
+		        <div class="content">
+					${this.content}
+		         </div>
+		         <p onclick="app.click_color(event,${this.id})">hi<!--i class="fa fa-search"--></i></p>
+		        <button onclick="app.delete_note(${this.id})" class="delete">Delete</button>
 		    </div>
 		    `
-	    )
     }
 }
 
 class App {
+
     constructor() {
         this.vault = window.localStorage
 	this.cards = []
@@ -32,24 +31,39 @@ class App {
     create_note() {
         const title = document.getElementById('title').value
         const content = document.getElementById('content').value
-        const card = new Card(this.card_count, title, content)
-	this.card_count++
-	this.cards.push(card)
-	this.render()
-    }
-    delete_note(id) {
-	for (let i = 0; i < this.cards.length; i++) {
-		if (this.cards[i].id == id) {
-			this.cards.splice(i, 1)
+		if (title !== '' && content !== '') {
+			const card = new Card(this.card_count, title, content)
+			this.card_count++
+			this.cards.push(card)
+			this.render()
 		}
-	}
-	this.render()
+		else {
+
+		}
     }
-    render() {
-	document.getElementById('notes').innerHTML = ``
-	for (const card of this.cards) {
-		document.getElementById('notes').innerHTML += card.html()
+
+    delete_note(id) {
+		for (let i = 0; i < this.cards.length; i++) {
+			if (this.cards[i].id === id) {
+			this.cards.splice(i, 1)
+			}
+		}
+		this.render()
+    }
+
+    click_color(event) {
+    	let x = event.clientX;
+    	let y = event.clientY;
+		let palette = document.getElementById('color-palette');
+		palette.style.visibility = 'visible';
+		palette.style.top = `${y}px`;
+		palette.style.left = `${x}px`;
 	}
+    render() {
+		document.getElementById('notes').innerHTML = ``
+		for (const card of this.cards) {
+			document.getElementById('notes').innerHTML += card.html()
+		}
     }
 }
 
