b5c720f00dd2c9a9a7f44931249eb3bdcdefdd92
Alessandro Chumpitaz <par_214@hotmail.com>
2020-06-19 19:44:35 -0500

    Merge joaquin with alessandro

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 class Card {
-    constructor(id, title, content){
+    constructor(id, title, content, color){
         this.id = id;
         this.title = title;
         this.content = content;
         this.pinned = false;
+		this.color = color;
     }
     html() {
 	    return `
-		    <div class="card" onclick="app.edit_card(${this.id}">
-		    	<div class="title">
-					${this.title}
-		        </div>
-		        <div class="content">
-					${this.content}
-		         </div>
-		         <p onclick="app.showPalette(event,${this.id})"><i class="fas fa-palette"></i></p>
-		        <button onclick="app.deleteNote(${this.id})" class="delete">Delete</button>
+		    <div class="card" style="background: ${this.color}">
+		    	<div class="card-body" onclick="app.edit_card(${this.id})">
+		    		<div class="title">${this.title}</div>
+		    		<div class="content">${this.content}</div>
+		    	</div>
+				<div class="card-options">
+					<p onclick="app.showPalette(event,${this.id})"><i sclass="fas fa-palette"></i></p>
+	        		<button onclick="app.deleteNote(${this.id})" class="delete">Delete</button>
+				</div>
 		    </div>
 		    `
     }
@@ -25,19 +26,19 @@ class App {
 
     constructor() {
         this.vault = window.localStorage
-	this.cards = []
-	this.card_count = 0
-	this.new_note_color = '#FFFFFF'
+		this.cards = []
+		this.card_count = 0
+		this.new_note_color = '#FFFFFF'
     }
     createNote() {
         const title = document.getElementById('title').value
         const content = document.getElementById('content').value
-	if (title !== '' && content !== '') {
-		const card = new Card(this.card_count, title, content, this.new_note_color)
-		this.card_count++
-		this.cards.push(card)
-		this.render()
-	}
+		if (title !== '' && content !== '') {
+			const card = new Card(this.card_count, title, content, this.new_note_color)
+			this.card_count++
+			this.cards.push(card)
+			this.render()
+		}
     }
 
     deleteNote(id) {
@@ -55,7 +56,7 @@ class App {
 	} else {
 		this.new_note_color = color
 	}
-	document.getElementById('color-palette').style.display = 'none'
+	document.getElementById('color-palette').style.visibility = 'hidden'
 	this.render()
     }
 
@@ -79,9 +80,9 @@ class App {
 				this.injectColor(color, obj)
 			}
 		}
-		palette.style.display = 'block';
-		palette.style.top = `${event.clientX}px`;
-		palette.style.left = `${event.clientY}px`
+		palette.style.visibility = 'visible';
+		palette.style.left = `${event.clientX}px`;
+		palette.style.top = `${event.clientY}px`
 
 	}
 
@@ -96,10 +97,18 @@ class App {
 	}
 
 	edit_card(id) {
-		console.log(id);
+
+		let obj = null
+		for (let i = 0; i < this.cards.length; i++) {
+			if (this.cards[i].id === id) {
+				obj = this.cards[i]
+			}
+		}
+
 		let modal = document.getElementById('myModal');
 		let modal_body = modal.children[0];
 		modal.style.display = 'block';
+		modal.children[0].style.backgroundColor = `${obj.color}`
 
 		window.onclick = (event) => {
 			if (event.target === modal){
