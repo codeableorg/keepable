9917e89550f723c8a1c220cbc871b0d54efbb53f
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-06-19 22:15:35 -0500

    user can include custom color while creating a note

--- js/note.js
+++ js/note.js
@@ -29,10 +29,28 @@ class Note {
     div.innerHTML = `
       <p>${this.body}</p>
       <div class="buttons">
-        <div class="palette"><img src="images/palette.svg" alt="palette"></div>
-        <div class="trash"><img src="images/trash2.svg" alt="trash"></div>
+        <div class="button-palette"><img src="images/palette.svg" alt="palette"></div>
+        <div class="button-trash"><img src="images/trash2.svg" alt="trash"></div>
       </div>
     `;
     return div;
+  }
+
+  static createButtonPalette() {
+    const a = document.createElement('A');
+    a.classList.add('button-palette');
+    a.href = '#';
+    a.innerHTML = `
+      <img class="button-palette__image" src="images/palette.svg" alt="palette">
+      <div class="button-palette__colors">
+        ${Note.colors.map((color) => `
+          <label>
+            <div class="button-palette__color" style="background:${color}"></div>
+            <input class="create-note__radio" type="radio" name="color" value="${color}">
+          </label>
+        `).join('')}
+      </div>
+    `;
+    return a;
   }
 }
